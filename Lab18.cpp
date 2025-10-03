79b94ddd82c75629470bb95498bf8cc044e77e97
mansoornoori18 <mnoori926@insite.4cd.edu>
2025-10-03 12:25:06 -0700

    update

--- Lab18.cpp
+++ Lab18.cpp
@@ -12,11 +12,23 @@ struct ReviewNode{
 };
 // Function to add a new review node at the head of the linked list
 void addAtHead(ReviewNode*& head, double rating, const string& comment){
-    ReviewNode* newNode = new ReviewNode{    // Create new node, next points to current head
-        rating, comment, head                   
-    };    
-    head = newNode;       // Update head pointer to the new node
+    ReviewNode* newNode = new ReviewNode{rating, comment, head}; // Create new node, next points to current head
+        head = newNode;       // Update head pointer to the new node
 }
+// Function to add a new review node at the tail of the linked list
+void addAtTail(ReviewNode*& head, double rating, const string& comment){
+    ReviewNode* newNode = new ReviewNode{ rating, comment, nullptr }; // New node's next is nullptr because it will be last
+    if (head == nullptr){
+        head = newNode;      // If list is empty, new node becomes the head
+    } else {
+        // Otherwise, traverse to the end and add the new node there
+        ReviewNode* temp = head;
+        while (temp->next != nullptr)
+        temp = temp->next;
+        temp->next = newNode;
+    }
+}
+
 
 
 

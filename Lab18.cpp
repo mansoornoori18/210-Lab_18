1770d6306a0f4b54105fcb407142c9bb14856c35
mansoornoori18 <mnoori926@insite.4cd.edu>
2025-10-03 12:12:40 -0700

    update

--- Lab18.cpp
+++ Lab18.cpp
@@ -1,4 +1,4 @@
-#include<iostream>  // COMSC-210 | Lab 12 | Mansoor Noori
+#include<iostream>  // COMSC-210 | Lab 18 | Mansoor Noori
 #include<string>    // IDE Visual Studio,
 #include<iomanip>      
 
@@ -10,6 +10,13 @@ struct ReviewNode{
     string comment;      // Review comment
     ReviewNode* next;    // Pointer to the next node
 };
+// Function to add a new review node at the head of the linked list
+void addAtHead(ReviewNode*& head, double rating, const string& comment){
+    ReviewNode* newNode = new ReviewNode{    // Create new node, next points to current head
+        rating, comment, head                   
+    };    
+    head = newNode;       // Update head pointer to the new node
+}
 
 
 

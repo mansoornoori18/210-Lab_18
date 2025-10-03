d2d44f2e628d3c74d8973bd857a717e21f9460be
mansoornoori18 <mnoori926@insite.4cd.edu>
2025-10-03 12:50:49 -0700

    update

--- Lab18.cpp
+++ Lab18.cpp
@@ -48,13 +48,31 @@ void outputReviews(ReviewNode* head){
         total += current->rating;  // Sum ratings for average calculation
         current = current->next;
     }
-    
+    // Output average rating with 5 decimal places
+    cout << "    > Average: " << fixed << setprecision(5) << total / count << endl;
 }
-
-
-
-
+// Function to free all nodes in the linked list to avoid memory leaks
+void freeList(ReviewNode*& head){
+     while (head != nullptr){
+        ReviewNode* temp = head;
+        head = head->next;
+        delete temp;  // Delete the node
+     }
+}
+// main function
 int main (){
+    ReviewNode* head = nullptr;  // Initialize linked list head pointer to nullptr
+    int choice;
+
+    // Ask the user how to add new nodes (head or tail)
+    cout << "Which linked list method should we use?\n";
+    cout << "    [1] New nodes are added at the head of the linked list\n";
+    cout << "    [2] New nodes are added at the tail of the linked list\n";
+    cout << "Choice: ";
+    cin >> choice;
+    cin.ignore(); // Clear newline character from input buffer
+
+    // Validate user choice
 
 
     cout << "life is beautiful\n";

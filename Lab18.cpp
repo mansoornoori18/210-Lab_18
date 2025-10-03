73122c8a24cf8995084e30350145d5f5e6813b50
mansoornoori18 <mnoori926@insite.4cd.edu>
2025-10-03 12:00:24 -0700

    update

--- Lab18.cpp
+++ Lab18.cpp
@@ -1,11 +1,23 @@
-#include<iostream>
- 
+#include<iostream>  // COMSC-210 | Lab 12 | Mansoor Noori
+#include<string>    // IDE Visual Studio,
+#include<iomanip>      
+
 using namespace std;
+
+// struct 
+struct ReviewNode{
+    double rating;       // Review rating
+    string comment;      // Review comment
+    ReviewNode* next;    // Pointer to the next node
+};
+
+
 
 int main (){
 
 
+    cout << "life is beautiful\n";
 
     return 0;
-    
+
 }
